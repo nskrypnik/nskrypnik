09ce0683dc3fdf277137b0160d48ddbfb30d4360
Niko Skrypnik <nskrypnik@gmail.com>
2011-10-04 22:43:14 +0300

    Cosmetic changes

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ USE_L10N = True
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/site_media'
+MEDIA_URL = '/site_media/'
 
 STATIC_URL = MEDIA_URL
 
